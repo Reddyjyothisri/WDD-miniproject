c36dfd3192aaaf9b7ddf6b5e6677716bf416d9c9
Reddy jyothi sri D <102847159+Reddyjyothisri@users.noreply.github.com>
2023-04-28 12:31:17 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+    $Username = $_POST['Username'];
+    $Password = $_POST['Password'];
+    
+    //Database connection here
+    $con = new mysqli("localhost","root","Hanuman@99","wdd7b");
+     if()
+     ?>
